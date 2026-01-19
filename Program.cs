de1aca6699e53d896a0a86c02fc41a513993c327
Pauloama <omegapaulo70@gmail.com>
2026-01-19 06:08:49 -0300

    Correções da Azure

--- Program.cs
+++ Program.cs
@@ -65,11 +65,14 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
+
     app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("swagger/v1/swagger.json", "Biblioteca API v1");
+        options.RoutePrefix = "swagger";
+    });
+
 
 app.UseHttpsRedirection();
 
